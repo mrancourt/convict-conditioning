a9d17017d2256051a53f48ef017d3aa6d8c85e84
mrancourt <mrancourt@tesla.com>
2018-08-23 20:01:05 -0700

    bars

--- ProgressScreen.js
+++ ProgressScreen.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {
   Button, ScrollView, Text, View,
 } from 'react-native';
+import AnimatedBar from 'react-native-animated-bar';
 import PropTypes from 'prop-types';
 import dataStore from './datastore';
 
@@ -9,22 +10,38 @@ const ucFirst = string => (
   string.charAt(0).toUpperCase() + string.slice(1)
 );
 
+const progress = done => (
+  Math.round(done / 30 * 100)
+);
+
 class ProgressScreen extends Component {
+
+  static navigationOptions = {
+    title: 'Your Progress',
+  };
+
+
   render() {
     const { navigation } = this.props;
     return (
       <ScrollView>
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
           {Object.keys(dataStore['@muscleGroups']).map(muscleGroup => [
-            <Text key={`progression-${muscleGroup}`}>{`${muscleGroup} => ${dataStore[`@progress-${muscleGroup}`]}`}</Text>,
             <Button
               key={`nav-${muscleGroup}`}
               title={muscleGroup}
               onPress={() => navigation.navigate(ucFirst(muscleGroup))}
             />,
-            <View key={`progress-${muscleGroup}`} width="70%" style={{ margin: 10 }}>
-              <View width={`${dataStore[`@progress-${muscleGroup}`]}%`} height={20} style={{ backgroundColor: '#0c204a' }} />
-            </View>,
+            <AnimatedBar
+              key={`progress-${muscleGroup}`}
+              style={{margin: 10}}
+              progress={dataStore[`@progress-${muscleGroup}`]}
+              height={30}
+              barColor="#264653"
+              fillColor="#2A9D8F"
+              borderWidth={0}
+              animate={false}
+            />,
           ])}
         </View>
       </ScrollView>
